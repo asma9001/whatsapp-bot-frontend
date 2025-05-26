8cb7d927e585c149db60248587e4c12385ff2ca2
Asma Siddique <109345779+asma9001@users.noreply.github.com>
2025-05-26 09:44:29 +0500

    Update constant.jsx

--- src/pages/constant.jsx
+++ src/pages/constant.jsx
@@ -1,6 +1,6 @@
 const constant = {
-  apiUrl: "https://whatsapp-auto-bot.up.railway.app/api",
-  socketUrl: "https://whatsapp-auto-bot.up.railway.app",
+  apiUrl: "https://whatsapp-bot-backend.up.railway.app/api",
+  socketUrl: "https://whatsapp-bot-backend.up.railway.app",
 };
 
 export default constant;
